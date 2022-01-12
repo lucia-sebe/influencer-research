afcf83b9880a250aa729aa1ba66c9c919e1f157f
lucia-sebe <luli.se04@gmail.com>
2022-01-12 10:36:09 -0300

    primer commit

---
+++ app/src/pages/main.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Main() {
+    return (
+        <main className="w-screen h-screen font-'Source Sans Pro' ">
+            <header>
+                <h1>HOLA chau</h1>
+            </header>
+        </main>
+    )
+}
+
+export default Main;
