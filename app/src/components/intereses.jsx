dcc6a93bc949ec2012f143af591abd65397ce6b0
lucia-sebe <luli.se04@gmail.com>
2022-02-23 16:49:33 -0300

    HOME

---
+++ app/src/components/intereses.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import '../styles/home.css'
+
+function Intereses() {
+    return (
+        <div className="flex mx-auto grid 2xl:grid-cols-3 w-3/6 lg:grid-cols-2 md:grid-cols-1 lg:w-5/6">
+            <button className="interes button"> Videojuegos </button>
+            <button className="interes button"> Café</button>
+            <button className="interes button"> Comida</button>
+            <button className="interes button"> ASMR</button>
+            <button className="interes button"> Autos</button>
+            <button className="interes button"> Política</button>
+            <button className="interes button"> Relajación</button>
+            <button className="interes button"> Terror</button>
+            <button className="interes button"> Música</button>
+            <button className="interes button"> Viajes</button>
+            <button className="interes button"> Literatura</button>
+            <button className="interes button"> Comedia</button>
+            <button className="interes button"> Deportes</button>
+            <button className="interes button"> Cocina</button>
+            <button className="interes button"> Ciencia</button>
+            <button className="interes button"> Belleza</button>
+            <button className="interes button"> Animación</button>
+        </div>
+    )
+}
+
+export default Intereses;
--- app/src/pages/home.jsx
+++ app/src/pages/home.jsx
@@ -1,41 +1,53 @@
 import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
 import Logo from '../images/logolu.png'
 import Intereses from '../components/intereses'
+import { LogoutButton } from '../auth0/logout'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faBars } from '@fortawesome/free-solid-svg-icons'
 import { useAuth0 } from "@auth0/auth0-react";
-
 import '../styles/home.css'
+
+
 function Home() {
 
     const [menu, setMenu] = useState(false);
     const showMenu = () => setMenu(!menu);
+    const [button, setButton] = useState(false);
+    const showButton = () => setButton(!button);
     const { user, isAuthenticated } = useAuth0();
 
     return (
         isAuthenticated && (
-            <div className="w-screen h-screen bg-gray-200">
+            <div className="w-screen h-screen bg-gray-200 overflow-y-scroll">
                 <header className="h-20 flex ">
-                    <div className="w-1/2 flex">
-                        <FontAwesomeIcon icon={faBars} onClick={showMenu} size="2x" className="color m-4" />
-                        {/* <img src={user.picture} alt={user.name} className="h-2/5 mr-16 rounded-3xl" /> */}
-                        <h1 className="color font text-xl mt-5 ml-3 ">{ user.name }</h1>
+                    <div className="2xl:w-1/2 lg:w-1/4 usm:w-1/2 flex">
+                        <FontAwesomeIcon icon={faBars} onClick={showMenu} size="1x" className="color 2xl:m-4 md:m-4 usm:mt-6 usm:ml-4" />
+                        <h1 className="color font md:text-xl sm:text-l mt-5 ml-3 ">{user.name}</h1>
                     </div>
 
-                    <div className="w-1/2 flex justify-end">
-                        <h1 className="color font-bold text-xl mt-5 ">INFLUENCER RESEARCH</h1>
-                        <img src={Logo} alt="logo of influencer research" className="w-12 h-16 m-1 mr-6"></img>
+                    <div className="2xl:w-1/2 lg:w-3/4 usm:w-1/2 flex justify-end">
+                        <h1 className="color font-bold md:text-xl sm:text-l mt-5 ">INFLUENCER RESEARCH</h1>
+                        <img src={Logo} alt="logo of influencer research" className="md:w-12 md:h-16 md:m-1 md:mr-6 usm:w-6 usm:h-8 usm:mt-4 usm:ml-4 usm:mr-4"></img>
                     </div>
                 </header>
 
-                <div className={menu ? 'menu active' : 'menu'} ></div>
+                <div className={menu ? 'menu active' : 'menu'} >
+                    <p className="ml-4 mt-8 hover:text-gray-900">Edit profile</p>
+                    <p className="ml-4 mt-4 hover:text-gray-900">Intereses</p>
+                    <p className="ml-4 mt-4 hover:text-gray-900">Help</p>
+                    <div className="ml-4 mt-4 hover:text-gray-900"> <LogoutButton /></div>
+                </div>
 
-                <div>
-                    <Intereses />
+                <Intereses />
+
+                <div className="boxNextButton">
+                    <Link to='/mapa' className="nextButton allButton border rounded-full m-12 p-2 pr-8 pl-8 md:text-xl usm:text-md">NEXT</Link>
                 </div>
             </div>
         )
     )
+
 }
 
 export default Home;
