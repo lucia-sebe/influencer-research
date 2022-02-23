0498e863d37324f6c581bd409dbb842c35f06139
lucia-sebe <luli.se04@gmail.com>
2022-02-23 17:10:38 -0300

    MAPS

---
+++ app/src/components/mapsMarker.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Logo from '../images/logolu.png'
+
+function MapsMarker() {
+    return (
+            <img src={Logo} alt="logo of influencer research" className="md:w-6 md:h-8 md:mr-6 md:mb-6 usm:w-6 usm:h-8"></img>
+      
+    )
+}
+
+export default MapsMarker;
