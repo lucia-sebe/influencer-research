b200c5ed02f52891d96550f9e5c669a83c95ed4e
lucia-sebe <luli.se04@gmail.com>
2022-03-01 13:12:46 -0300

    Recomendaciones cambio en props

--- app/src/components/recomendaciones.jsx
+++ app/src/components/recomendaciones.jsx
@@ -1,32 +1,26 @@
 import React, { useState, useEffect } from 'react'
 
-const API_KEY = 'AIzaSyDEc047qDtqEJ65W0Tge6vs34A6jnubkfU',
-    BASE_URL = 'https://www.googleapis.com/youtube/v3/',
-    maxResults = 4,
-    order = "viewCount";
-
-
-const Recomendaciones = (props) => {
-
-    let q = "music",
-        //location = "6.321097%2C%20-10.807359",
-        location = (props.location[0].lat + "%2C%20" + props.location[0].lng),
-        locationRadius = "1000km";
-
-    let finalURLuno = `${BASE_URL}search?part=snippet&location=${location}&locationRadius=${locationRadius}&maxResults=${maxResults}&order=${order}&q=${q}&type=video&key=${API_KEY}`;
-
+const Recomendaciones = ({ location }) => {
+    const API_KEY = 'AIzaSyDEc047qDtqEJ65W0Tge6vs34A6jnubkfU';
+    const BASE_URL = 'https://www.googleapis.com/youtube/v3/';
+    const maxResults = 4;
+    const order = "viewCount";
+    const q = "music";
+    const locationRadius = "500km";
     const [resultyt, setResultyt] = useState([]);
-
+    const [queryLocation, setQueryLocation] = useState('6.321097%2C%20-10.807359');
+    const finalURL = `${BASE_URL}search?part=snippet&location=${queryLocation}&locationRadius=${locationRadius}&maxResults=${maxResults}&order=${order}&q=${q}&type=video&key=${API_KEY}`;
+    
     useEffect(() => {
-       traerVideos()
+        setQueryLocation(location[0]?.lat + "%2C%20" + location[0]?.lng);
+        traerVideos();
     }, [location]);
 
     const traerVideos = () => {
-        fetch(finalURLuno)
+        fetch(finalURL)
             .then((response) => response.json())
             .then((responseJson) => {
                 setResultyt(responseJson.items.map(obj => "https://www.youtube.com/embed/" + obj.id.videoId))
-                console.log("location-probando:  ", props.location[0].lat, "%2C%20", props.location[0].lng )
             })
             .catch((error) => {
                 console.error(error);
